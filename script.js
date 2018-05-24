3703fd9f68acf578655b112eb3d53645ae05c01e
davidjkennedy <dkennedychicago@gmail.com>
2018-05-24 10:54:48 -0400

    added pseudo code to script file

---
+++ script.js
@@ -0,0 +1,12 @@
+// Psuedo Code
+// 1
+// Set up the variables
+
+// 2
+// On page load set the value of base is reset to 0
+
+// 3
+// when plus button is clicked 1 is added to base and new value is assigned to base
+
+// 4
+// When minus button  is clicked 1 is subtracted from base new value is assigned to base
